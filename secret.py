ebb3f5915139f92ea847d5d39fee311ae6391010
David S Colgan <david@davidscolgan.com>
2013-11-06 12:05:12 -0500

    oh crap I pushed the secret key, must now change them

--- secret.py
+++ secret.py
@@ -1,4 +1,9 @@
 SECRET_KEY = 'cf9(f9u69ue(p_01qp_!!1&%cz^&a7@0z+i9flfhb1dxio9z9^'
 POSTMARK_API_KEY = '2dcc4c9d-8876-40ad-adfa-09fd580df841'
+ADMINS = (
+    ('David Colgan', 'dvcolgan@gmail.com'),
+)
+SERVER_EMAIL = 'david@lessboring.com'
+EMAIL_HOST = 'smtp.postmarkapp.com'
 
 #http://lookwhatimadeweekend.us7.list-manage.com/subscribe/post?u=d0ff99cbf1c7c20ff56103b8d&amp;id=d226a5b1be
