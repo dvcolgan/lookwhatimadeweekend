84f40aa718052ddc02fc89daee26d2c062c429ed
David Colgan <dvcolgan@gmail.com>
2014-03-08 13:09:34 -0500

    fix

--- lwimw/context_processors.py
+++ lwimw/context_processors.py
@@ -5,7 +5,7 @@ from django.utils import timezone
 def common(request):
     contests = Contest.objects.order_by('-start')
     for contest in contests:
-        contest.can_vote = request.user.is_authenticated() and user_can_vote(request.user, Submission.objects.all())
+        contest.can_vote = (request.user.is_authenticated() and Submission.objects.filter(user=request.user).count() > 0)
     current_contest = contests[0]
     #now = timezone.now()
 
