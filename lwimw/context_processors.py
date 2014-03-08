cc3c8e0f77c2e9194606957ed80a4d96fc5e89c2
David Colgan <dvcolgan@gmail.com>
2014-03-08 13:05:07 -0500

    anyone can vote who submitted in the past

--- lwimw/context_processors.py
+++ lwimw/context_processors.py
@@ -1,11 +1,11 @@
-from lwimw.models import Contest, user_can_vote
+from lwimw.models import Contest, user_can_vote, Submission
 from django.utils import timezone
 
 
 def common(request):
     contests = Contest.objects.order_by('-start')
     for contest in contests:
-        contest.can_vote = request.user.is_authenticated() and user_can_vote(request.user, contest.submissions.all())
+        contest.can_vote = request.user.is_authenticated() and user_can_vote(request.user, Submission.objects.all())
     current_contest = contests[0]
     #now = timezone.now()
 
