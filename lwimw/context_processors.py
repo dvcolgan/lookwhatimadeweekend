798bd6f1ecb1a14374797dbec2ea836d6cac7a4d
Corstiaan Hol <corstiaan.hol@gmail.com>
2014-04-29 02:02:28 +0200

    Update context_processors.py
    Updated the context processor to not crash with an IndexError when no contests exists.

--- lwimw/context_processors.py
+++ lwimw/context_processors.py
@@ -6,6 +6,10 @@ def common(request):
     contests = Contest.objects.order_by('-start')
     for contest in contests:
         contest.can_vote = request.user.is_authenticated() and user_can_vote(request.user, contest.submissions.all())
+    if contests:
+        current_contest = contests[0]
+    else:
+        current_contest = None
     current_contest = contests[0]
     #now = timezone.now()
 
